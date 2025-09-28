80ebc8c24260be551776fbec7c38ace0ab81bc26
Abhijeet Sonar <abhijeet.nkt@gmail.com>
2025-09-28 14:56:28 +0530

    nvim: basic setup

---
+++ .config/nvim/init.lua
@@ -0,0 +1,25 @@
+vim.cmd('syntax enable');
+vim.cmd('colorscheme desert');
+
+-- Unset background color
+local groups = {
+    "Normal",
+    "NormalNC",
+    "NonText",
+    "LineNr",
+    "EndOfBuffer"
+}
+for _, group in ipairs(groups) do
+  vim.api.nvim_set_hl(0, group, { bg = "none" })
+end
+
+vim.o.number = true;
+vim.o.relativenumber = true;
+
+-- Make tabs look 4 characters wide
+vim.o.tabstop = 4;
+-- Indent with 4 spaces
+vim.o.shiftwidth = 4;
+-- Expand tabs to spaces
+vim.o.expandtab = true;
+
